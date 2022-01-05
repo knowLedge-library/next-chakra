cd9065b4d510133256bffa75b94097eb248098df
iseekTo <1214046231@qq.com>
2022-01-05 14:35:55 +0800

    package: add @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -1,43 +1,51 @@
 const path = require("path");
+const nextBundleAnalyzer = require("@next/bundle-analyzer");
 const withTM = require("next-transpile-modules");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+// Enable analyze
+const withBundleAnalyzer = nextBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
 /** @type { import('next').NextConfig } */
 module.exports = withTM([
   "@cosmosreverse/cosmos",
   "@cosmosreverse/cosmos-react",
-])({
-  reactStrictMode: true,
+])(
+  withBundleAnalyzer({
+    reactStrictMode: true,
 
-  webpack(config, options) {
-    const { dev, isServer } = options;
+    webpack(config, options) {
+      const { dev, isServer } = options;
 
-    // Do not run type checking twice
-    // Show type error in runtime
-    if (dev && isServer) {
-      config.plugins.push(new ForkTsCheckerWebpackPlugin());
-    }
+      // Do not run type checking twice
+      // Show type error in runtime
+      if (dev && isServer) {
+        config.plugins.push(new ForkTsCheckerWebpackPlugin());
+      }
 
-    // 使用 `yarn link` 时，避免使用重复的React依赖所引发的错误。
-    // For more info @see https://github.com/martpie/next-transpile-modules/tree/2d935beb70f8420ca42dd0d7bb2ca6755ff8d38c#i-have-trouble-with-duplicated-dependencies-or-the-invalid-hook-call-error-in-react
-    config.resolve.alias["react"] = path.resolve(
-      __dirname,
-      "node_modules/react"
-    );
+      // 使用 `yarn link` 时，避免使用重复的React依赖所引发的错误。
+      // For more info @see https://github.com/martpie/next-transpile-modules/tree/2d935beb70f8420ca42dd0d7bb2ca6755ff8d38c#i-have-trouble-with-duplicated-dependencies-or-the-invalid-hook-call-error-in-react
+      config.resolve.alias["react"] = path.resolve(
+        __dirname,
+        "node_modules/react"
+      );
 
-    return config;
-  },
+      return config;
+    },
 
-  webpackDevMiddleware(config) {
-    // eslint-disable-next-line no-param-reassign
-    config.watchOptions.ignored = [
-      // remove ignoring of all npm modules
-      ...config.watchOptions.ignored.filter(
-        (ignore) => !ignore.toString().includes("node_modules")
-      ),
-      /[\\/]node_modules[\\/]((?!dist|dist-react|loader)[\\/].+)/,
-    ];
+    webpackDevMiddleware(config) {
+      // eslint-disable-next-line no-param-reassign
+      config.watchOptions.ignored = [
+        // remove ignoring of all npm modules
+        ...config.watchOptions.ignored.filter(
+          (ignore) => !ignore.toString().includes("node_modules")
+        ),
+        /[\\/]node_modules[\\/]((?!dist|dist-react|loader)[\\/].+)/,
+      ];
 
-    return config;
-  },
-});
+      return config;
+    },
+  })
+);
